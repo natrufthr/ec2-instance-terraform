f1a4065dd63a4b6b64956ddab27fa3dce4bde00e
osboxes.org <natrufthr@gmail.com>
2022-08-02 11:43:39 -0400

    Adding files

---
+++ main.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+variable "ec2_instance_name" {}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = var.ec2_instance_name
+  }
+}
